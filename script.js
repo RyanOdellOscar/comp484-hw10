527881609ea8d7dd710bdc79267389bef50c9ddc
RyanOdellOscar <ryan.oscar.490@my.csun.edu>
2026-05-05 16:52:51 -0700

    Update script.js

--- script.js
+++ script.js
@@ -8,21 +8,8 @@ $(function() { // Makes sure that your function is called once all the DOM eleme
     $('.play-button').click(clickedPlayButton);
     $('.exercise-button').click(clickedExerciseButton);
     $('.attack-button').click(clickedAttackButton);
-    $('.name-input').change(function() {
-      current_pet = new Pet($('.name-input').val() || "Pet");
-      if(number_of_pets >= 6) {
-        index = replace_pet(current_pet);
-        document.getElementById("pet-comment").innerHTML = current_pet.name + "has been added. " + pet_array[index].name + " has been removed.";
-      }
-      else{index = number_of_pets;
-        number_of_pets++;
-      }
-      pet_array[index] = current_pet;
-      pet_info = current_pet;
-      checkAndUpdatePetInfoInHtml();
-    });
-  
 
+  
   })
   
     // Add a variable "pet_info" equal to a object with the name (string), weight (number), and happiness (number) of your pet
@@ -30,9 +17,48 @@ $(function() { // Makes sure that your function is called once all the DOM eleme
     var number_of_pets = 0;
     var punching_bag_health = 100;
     var bag_level = 1;
+    const style = "color: blue; font-size: 16px";
     // Hide punching bag until attack button is clicked
     $('.punching-bag-image').hide();
-    var pet_info = {name: $('.name-input').val() || "Pet", weight:1, happiness:0 };
+    var pet_info = {name: "Pet", weight:1, happiness:0 };
+    const nameBox = document.querySelector('.name-input');
+    nameBox.addEventListener('click', function(event){
+      const duration = 5000;
+      const start = new Date().getTime();
+      while (new Date().getTime() < start + duration) {
+    // Block the main thread for 3 seconds.
+      }})
+    //names a pet when user types in name input box and presses enter
+    nameBox.addEventListener('keypress', function(event){
+      if(event.key === 'Enter') {
+        current_pet = new Pet(nameBox.value || "Pet");
+        
+        if(number_of_pets == 6) {
+          index = replace_pet(current_pet);
+          console.error("Maximum pets reached!");
+        document.getElementById("pet-comment").innerHTML = current_pet.name + " has been added. " + pet_array[index].name + " has been removed.";
+        console.table(pet_array);
+      }
+      else{
+        index = number_of_pets;
+        number_of_pets += 1;
+        console.log("I have " + number_of_pets + " pets.");
+        console.warn("I can only have 6 pets, so I will have to replace one if I want to add more.");
+        console.table(pet_array);
+      }
+      pet_array[index] = current_pet;
+      pet_info = current_pet;
+      console.log("%cNew pet added!", style);
+      console.group();
+      for(var i = 0; i < number_of_pets; i++){
+        console.log("This is pet number " + (i + 1) + " named " + pet_array[i].name);
+      }
+      console.groupEnd();
+      console.table(pet_array);
+      checkAndUpdatePetInfoInHtml();
+
+    }
+  });
 
     //pet contructor function
     function Pet(name) {
@@ -84,11 +110,13 @@ $(function() { // Makes sure that your function is called once all the DOM eleme
       if (pet_info.weight < 0) {
         pet_info.weight = 0;
         alert("Your pet is too skinny! Please feed it some treats.");
+        console.error("pet weight is 0!");
       }
       // Add conditional so if happiness is lower than zero.
       if (pet_info.happiness < 0) {
         pet_info.happiness = 0;
         alert("Your pet is sad! Please play with it.");
+        console.warn("pet happiness is 0!");
       }
 
       if(pet_info.happiness <= 0 && pet_info.weight == 0) {
@@ -146,4 +174,26 @@ $(function() { // Makes sure that your function is called once all the DOM eleme
       alert(pet_list);
     }
 
-  
+
+
+// cause 404. Fetch a file that does not exist
+fetch('nonexistentfile.txt')
+  .then(response => response.text())
+  .then(data => console.log(data))
+  .catch(error => console.error('Error fetching file:', error));
+
+// cause TypeError. Call a non-function
+try {
+  var notAFunction = {};
+  notAFunction();
+} catch (error) {
+  console.error('TypeError:', error);
+}
+
+// cause Violation. Use setTimeout with a long delay
+
+const duration = 5000;
+const start = new Date().getTime();
+while (new Date().getTime() < start + duration) {
+    // Block the main thread for 3 seconds.
+}
